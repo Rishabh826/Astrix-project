ada2bf4e8f013d0723695fdbac8df1b09767193b
Rishabh826 <rishabhjangid0707@gmail.com>
2024-05-18 17:51:32 +0530

    hashrouter

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
-import { BrowserRouter, Route, Routes} from 'react-router-dom';
+import { HashRouter, Route, Routes} from 'react-router-dom';
 import './App.css';
 import Collection from './components/collection';
 import Event from './components/Event';
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
       <Route exact path="/" element={<Collection />} />
       <Route exact path="/event" element={<Event/>} />
       </Routes>
-      </BrowserRouter>
+      </HashRouter>
 
   );
 }
